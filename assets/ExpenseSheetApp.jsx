79d37de7d2791e65b78d69c9a24282255c7b4005
Janosch Hoffmann <janosch.hoffmann@check24.de>
2024-01-27 08:51:53 +0100

    Add react

---
+++ assets/ExpenseSheetApp.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function ExpenseSheetApp() {
+    return (
+        <div>
+            <h1>Expense Sheet</h1>
+            <p>More to come</p>
+        </div>
+    );
+}
--- assets/app.js
+++ assets/app.js
@@ -5,5 +5,14 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+import React from "react";
+import {createRoot} from "react-dom/client";
+import {ExpenseSheetApp} from "./ExpenseSheetApp";
+
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+
+const domNode = document.getElementById("expense-sheet-app");
+const root = createRoot(domNode);
+
+root.render(<ExpenseSheetApp />)
